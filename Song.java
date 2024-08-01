5bcf3293dba47c16c0f77db71104b0b47bc694a3
sakshi-nimje <115687946+sakshi-nimje@users.noreply.github.com>
2024-08-01 12:10:01 +0530

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+
+public class Song {
+    private String id;
+    private String title;
+    private String artist;
+
+    public Song(String id, String title, String artist) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(id, song.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
